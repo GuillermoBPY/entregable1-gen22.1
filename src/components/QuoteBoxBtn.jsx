f6a395b1c67cac087b629dee84b15c51b957bef5
GuillermoBPY <guillermo-benitez@outlook.com>
2023-01-20 17:04:42 -0300

    actr

--- src/components/QuoteBoxBtn.jsx
+++ src/components/QuoteBoxBtn.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-const QuoteboxBtn = ({ updateSates,color,saludo}) => {
+const QuoteboxBtn = ({ updateSates,color}) => {
   return (
     <button  style={{backgroundColor:color}} className="quotebox__btn" onClick={updateSates}>
       <i className="bx bx-chevron-right"></i>
